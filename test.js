68a2449f219c18f0614ed54e61d92584764cd6b1
Vishvendra Tomar <vishvendratomar29@gmail.com>
2023-07-22 00:18:38 +0530

    update on scroll trigger

--- test.js
+++ test.js
@@ -1,13 +1,37 @@
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        console.log(entry)
-        if(entry.isIntersecting){
+// const observer = new IntersectionObserver((entries)=>{
+//     entries.forEach((entry)=>{
+//         console.log(entry)
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show');
+//         }else{
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
+
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el)=>observer.observe(el));
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        }else{
+        } else {
             entry.target.classList.remove('show');
         }
     });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el)=>observer.objerve(el));
+hiddenElements.forEach((el) => observer.observe(el));
+
+const observers = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, { threshold: 0.2 });
+
+const cards = document.querySelectorAll('.card');
+cards.forEach((card) => observer.observe(card));
